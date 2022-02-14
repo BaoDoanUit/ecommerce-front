e1f565d724793dffc001223d03874a3b214ceae3
BaoDoanUit <baodoanuit@gmail.com>
2022-02-14 10:52:09 +0700

    add production

---
+++ build/precache-manifest.04eeb2145eaf3d91520fd6778827661f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c94c47eb1690c2140e1453cb5f7fea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9393d0d170979f6446c4",
+    "url": "/static/css/main.0a474689.chunk.css"
+  },
+  {
+    "revision": "add00df4e7b864e57bff",
+    "url": "/static/js/2.de631e74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.de631e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9393d0d170979f6446c4",
+    "url": "/static/js/main.34a69316.chunk.js"
+  },
+  {
+    "revision": "8b70eb2e35dee98fb26d",
+    "url": "/static/js/runtime-main.d33b66ca.js"
+  }
+]);
